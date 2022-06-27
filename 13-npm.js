9c44138f1c7e8c7be8a7ae51bfd848e7388112d6
“Joe <mulljose@us.ibm.com>
2022-06-26 18:37:39 -0700

    Check in of tutorial files to date

---
+++ 13-npm.js
@@ -0,0 +1,21 @@
+// npm - global command
+// npm --version
+
+//local dependency (for this project)
+// npm i <packageName>
+
+//global depedency - use on any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
+
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json, create properties, etc)
+// npm init (step by step, enter to skip)
+// npm init -y (everything default)
+
+const _ = require('lodash')
+
+const items = [1, [2, [3, 4]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
+console.log('Hello People.')
--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-// npm - global command
-// npm --version
+const { readFile, writeFile } = require('fs').promises
+//const util = require('util')
+//Let's say you want to read 2 files, then write one
 
-//local dependency (for this project)
-// npm i <packageName>
+//const readFilePromise = util.promisify(readFile)
+//const writeFilePromise = util.promisify(writeFile)
+//const getText = (path) => {
+//  return new Promise((resolve, reject) => {
+//    readFile(path, 'utf-8', (err, data) => {
+//      if (err) {
+//        reject(err)
+//      } else {
+//        resolve(data)
+//      }
+//    })
+//  })
+//}
 
-//global depedency - use on any project
-//npm install -g <packageName>
-// sudo npm install -g <packageName> (mac)
+//getText('./content/first.txt')
+//  .then((result) => console.log(result))
+//  .catch((err) => console.log(err))
 
-// package.json - manifest file (stores important info about project/package)
-// manual approach (create package.json, create properties, etc)
-// npm init (step by step, enter to skip)
-// npm init -y (everything default)
+const start = async () => {
+  try {
+    const first = await readFile('./content/first.txt', 'utf-8')
+    const second = await readFile('./content/second.txt', 'utf-8')
+    await writeFile(
+      './content/result-mind-grenade.txt',
+      `THIS IS AWESOME : ${first} ${second}`,
+      { flag: 'a' }
+    )
+    console.log(first, second)
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-const _ = require('lodash')
-
-const items = [1, [2, [3, 4]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
+start()
